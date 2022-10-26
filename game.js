0c585447f948a6fd3b28d9037dd1232e519d19cf
Steve Sajeev <stevepsajeev2008@gmail.com>
2022-10-27 04:07:29 +0530

    stars added to bg

--- game.js
+++ game.js
@@ -4,6 +4,14 @@ class Game {
         this.ctx = ctx;
         this.rocket = new Rocket();
 
+        this.stars = [];
+        for(let i = 0; i < 40; i++){
+            this.stars.push({
+                x:Math.random()*canvas.width,
+                y:Math.random()*canvas.height,
+                radius:Math.random()+0.2,
+            });
+        }
     }
 
     update(){
@@ -20,6 +28,18 @@ class Game {
         ctx.fillStyle = "white";
         ctx.strokeStyle = "white";
 
+        this.stars.forEach((s)=>{
+            s.x += s.radius/12;
+            s.y += s.radius/8;
+            if(s.x > canvas.width){s.x = 0}
+            if(s.x < 0){s.x = canvas.width}
+            if(s.y > canvas.height){s.y = 0}
+            if(s.y < 0){s.y = canvas.height}
+
+            ctx.beginPath();
+            ctx.arc(s.x,s.y,s.radius,0,Math.PI*2);
+            ctx.fill();
+        });
 
         this.rocket.draw();
     }
