9ef27cafc19bb9d7293e8778de418d0ef0255b32
Steve Sajeev <stevepsajeev2008@gmail.com>
2025-05-05 14:43:08 +0530

    Small updates

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const info = document.getElementById("info");
 let key = [];
+let mouse = {
+    x: 0,
+    y: 0,
+    left: false,
+    right: false
+}
 
 function main(){
     handleResize(); // init browser window sizing with canvas
-    let game = new Game(ctx);
+    let game = new Game(canvas, ctx);
     window.game = game;
     game.update();
 }
@@ -24,12 +30,35 @@ const handleKeyup = (e) => {
 }
 
 const handleMouseDown = (e) => {
-	// Adds camera pos to translate screen position to world position
-	let angle = Utils.getAngleBetweenPoints(game.rocket.p, new Vector(e.clientX + game.camera.p.x, e.clientY + game.camera.p.y));
-	//game.rocket.shoot(angle);
+    if(e.buttons == 0){
+        mouse.left = false;
+        mouse.right = false;
+    } else if(e.buttons == 1){
+        mouse.left = true;
+    } else if (e.buttons == 2){
+        mouse.right = true;
+    } else if (e.buttons == 3){
+        mouse.left = true;
+        mouse.right = true;
+    }
 }
-
+const handleMouseMove = (e) => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+    console.log(mouse.left, mouse.right)
+}
 const handleMouseUp = (e) => {
+    if(e.buttons == 0){
+        mouse.left = false;
+        mouse.right = false;
+    } else if(e.buttons == 1){
+        mouse.left = true;
+    } else if (e.buttons == 2){
+        mouse.right = true;
+    } else if (e.buttons == 3){
+        mouse.left = true;
+        mouse.right = true;
+    }
 }
 
 window.addEventListener("load", main);
@@ -37,5 +66,6 @@ window.addEventListener("resize", handleResize);
 window.addEventListener("keydown", handleKeydown);
 window.addEventListener("keyup", handleKeyup);
 window.addEventListener("mousedown", handleMouseDown);
+window.addEventListener("mousemove", handleMouseMove);
 window.addEventListener("mouseup", handleMouseUp);
 
--- game.js
+++ game.js
@@ -2,29 +2,43 @@ class Game {
 	prevstamp = 0;
 	initialStamp = 0;
 	lastrocktime = 0;
-    constructor(ctx){
-
+    constructor(canvas, ctx){
+		this.canvas = canvas;
         this.ctx = ctx;
         this.rocket = new Rocket(this);
         this.stars = [];
         this.rocks = [];
 		// Camera is offset by half of screen size to set the camera focussed at rocket, which is at (0,0)
 		// Camera position is of top left corner of camera size
-		this.camera = new Camera(-canvas.width/2,-canvas.height/2,canvas.width,canvas.height,ctx);
-		this.ui = new UI(ctx)
+		this.camera = new Camera(-this.canvas.width/2,-this.canvas.height/2,this.canvas.width,this.canvas.height,ctx);
+		this.renderer = new Renderer(this, ctx)
 		this.xx = 10;
 		this.xy = +1;
 
+		//currentState = "home"; // One of home, game, paused
+		this.gameStarted = false;
+		this.homeButtons = [
+			{
+				p: new Vector(300,400),
+				width: 100,
+				height: 100,
+				text: "Start Game",
+				action: ()=>{
+					this.gameStarted = true;
+				}
+			}
+		]
+
         for(let i = 0; i < 100; i++){
             this.stars.push({
-				p: new Vector(  Math.random()*canvas.width, Math.random()*canvas.height  ),
+				p: new Vector(  Math.random()*this.canvas.width, Math.random()*this.canvas.height  ),
 				radius:0.3+Math.pow(((Math.random()*4)), 4)/400,
             });
         }
 
 		// Initial rocks
         for(let i = 0; i < 8; i++){
-            this.rocks.push(new Rock(this, new Vector(Math.random()*canvas.width, Math.random()*canvas.height)));
+            this.rocks.push(new Rock(this, new Vector(Math.random()*this.canvas.width, Math.random()*this.canvas.height)));
         }        
     }
 
@@ -42,42 +56,50 @@ class Game {
 
 
 
+		if(!this.gameStarted){
+			this.homeButtons.forEach((btn)=>{
+				if(this.mouse){
 
-		// Object updates
-  	    this.rocket.update(delta);
+				}
+			})
 
-		// Update rocks
-		let countRocksNear = 0; // Number of rocks closeby
-		for(let i = 0; i < this.rocks.length; i++){
-			let r = this.rocks[i];
-			r.update();
+		} else if(this.gameStarted){
+
+			// Object updates
+			this.rocket.update(delta);
+
+			// Update rocks
+			let countRocksNear = 0; // Number of rocks closeby
+			for(let i = 0; i < this.rocks.length; i++){
+				let r = this.rocks[i];
+				r.update();
 
 
-			// A^2 + B^2 = C^2
-			// Distance between rock and rocket  (not squared)
-			// So just hard code a target distance that isnt rooted, so a large number
-			let dist = (this.rocket.p.x-r.p.x)**2 + (this.rocket.p.y-r.p.y)**2
+				// A^2 + B^2 = C^2
+				// Distance between rock and rocket  (not squared)
+				// So just hard code a target distance that isnt rooted, so a large number
+				let dist = (this.rocket.p.x-r.p.x)**2 + (this.rocket.p.y-r.p.y)**2
 
-			if(dist < canvas.width**2){
-				countRocksNear += 1;
+				if(dist < this.canvas.width**2){
+					countRocksNear += 1;
+				}
+				if(dist > (this.canvas.width*2)**2){
+					this.rocks.splice(i,1);
+					i -= 1;
+				}
 			}
-			if(dist > (canvas.width*2)**2){
-				this.rocks.splice(i,1);
-				i -= 1;
+			// Adding more rocks to scene
+			if(countRocksNear < 20){
+				this.rocks.push(new Rock(this))
 			}
+
+
+			// Camera stick to rocket
+			//this.camera.target = this.rocket.p.copy();
+			this.camera.target = new Vector(0,0);
 		}
-
-
-
-		// Adding more rocks to scene
-		// this.rocks.push(new Rock(this));
-		if(countRocksNear < 20){
-			this.rocks.push(new Rock(this))
-		}
-
 
 		// Camera
-		this.camera.target = this.rocket.p.copy();
 		this.camera.update();
 
 
@@ -89,7 +111,9 @@ class Game {
 		ctx.fillText("FPS " + this.fps, 10, 40);
 		ctx.fillText("xx " + this.xx, 10, 50);
 		ctx.fillText("xy " + this.xy, 10, 60);
-		ctx.fillText("rocksNear " + countRocksNear, 10, 70);
+		if(this.gameStarted){
+			ctx.fillText("rocksNear " + countRocksNear, 10, 70);
+		}
 
         requestAnimationFrame(this.update.bind(this));
 		/* Debugging: Simulate fps (low and high framerate)
@@ -105,60 +129,14 @@ class Game {
     draw(delta){
 
 		this.ctx.fillStyle = "black";
-        this.ctx.fillRect(0,0,canvas.width,canvas.height);
+        this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
         this.ctx.fillStyle = "white";
         this.ctx.strokeStyle = "white";
 
-        this.stars.forEach((star)=>{
-			let sPos = this.camera.calcPosParallax(star.p, 1/star.radius); // Calculates the positions on the screen according to camera
-			if(star.lastCalcPos == undefined){
-				star.lastCalcPos = sPos;
-			}
-            if(sPos.x > canvas.width){
-				star.p.x -= canvas.width
-				star.lastCalcPos.x -= canvas.width; // for the trailing effect also to work when the star crosses the borders
-				sPos.x -= canvas.width;
-			};
-            if(sPos.x < 0){
-				star.p.x += canvas.width
-				star.lastCalcPos.x += canvas.width;
-				sPos.x += canvas.width;
-			};
-            if(sPos.y > canvas.height){
-				star.p.y -= canvas.height;
-				star.lastCalcPos.y -= canvas.height;
-				sPos.y -= canvas.height;
-			};
-            if(sPos.y < 0){
-				star.p.y +=  canvas.height;
-				star.lastCalcPos.y += canvas.height;
-				sPos.y += canvas.height;
-			};
+		this.renderer.drawGame(delta);
 
-            this.ctx.beginPath();
-            ctx.arc(sPos.x, sPos.y, star.radius,0,Math.PI*2);
-			this.ctx.fill();
 
-			if(star.radius > 1){
-				// Only trail for considerably big stars
-				this.ctx.beginPath();
-				this.ctx.moveTo(sPos.x, sPos.y);
-				this.ctx.lineTo(star.lastCalcPos.x, star.lastCalcPos.y);
-				this.ctx.stroke();
-			}
-            this.ctx.closePath();
-			
-			// For trail effect
-			star.lastCalcPos = sPos;
-        });
 
-        this.rocks.forEach((r)=>{
-            r.draw();
-			let dist = Math.sqrt((this.rocket.p.x-r.p.x)**2 + (this.rocket.p.y-r.p.y)**2)
-			this.camera.fillText(dist, r.p.x, r.p.y)
-        });
-
-        this.rocket.draw(delta);
     }
 }
 
